0528745caeb503f2e5e8f2be274e2d4bbe33c492
Gabriel Lomba <gabriellomba@hotmail.com>
2018-07-05 23:27:27 -0300

    Use port env variable when present to listen

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.post('/imageUrl', submissions.handleFaceRecognition);
 
 app.put('/image', submissions.handleImageSubmission(db));
 
-app.listen(3001, () => {
-  console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT || 3000}`);
 });
